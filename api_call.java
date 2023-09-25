a0f6caff30734307aed7492ba8cc88084b0d2604
Vishesh-2 <77899506+Vishesh-2@users.noreply.github.com>
2023-09-25 22:38:57 +0530

    Create api_call.java

---
+++ api_call.java
@@ -0,0 +1,19 @@
+class call {
+	public static void main(String[] args) {
+		HttpRequest request = HttpRequest.newBuilder()
+				.uri(URI.create("https://jokes-by-api-ninjas.p.rapidapi.com/v1/jokes"))
+				.header("X-RapidAPI-Host", "jokes-by-api-ninjas.p.rapidapi.com")
+				.header("X-RapidAPI-Key", "your-rapidapi-key")
+				.method("GET", HttpRequest.BodyPublishers.noBody())
+				.build();
+		HttpResponse<String> response = null;
+		try {
+			response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println(response.body());
+	}
+}
